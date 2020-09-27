ce6c822aaf853aa67c818eb775441991b6f0da43
Artsiom Biba <biba.artsiom@gmail.com>
2020-09-27 17:44:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { Navbar, Button, Nav, NavItem, Image, Container, Row, Col } from 'react-bootstrap';
+import './App.css';
+import Todo from './assets/todo.png'
+
+class App extends Component {
+
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    console.log(this.props)
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/" style={{ 'padding-top': '23px' }}>Todo List</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1}>
+                {
+                  isAuthenticated() && (
+                    <Button
+                      bsStyle="success"
+                      className="btn-margin"
+                      onClick={this.goTo.bind(this, 'manage')}
+                    >
+                      Manage Todos
+                    </Button>
+                  )
+                }
+
+              </NavItem>
+              <NavItem eventKey={2}>
+                {
+                  isAuthenticated() && (
+                    <Button
+                      bsStyle="success"
+                      className="btn-margin"
+                      onClick={this.goTo.bind(this, 'alltodos')}
+                    >
+                      All Todos
+                    </Button>
+                  )
+                }
+
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                {
+                  !isAuthenticated() && (
+                    <Button
+                      bsStyle="primary"
+                      className="btn-margin"
+                      onClick={this.login.bind(this)}
+                    >
+                      Log In
+                    </Button>
+                  )
+                }
+                {
+                  isAuthenticated() && (
+                    <Button
+                      bsStyle="danger"
+                      className="btn-margin"
+                      onClick={this.logout.bind(this)}
+                    >
+                      Log Out
+                    </Button>
+                  )
+                }
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        {
+          !isAuthenticated() && (
+            <div className="container">
+              <Container>
+                <Row>
+                  <Col md={2} mdPush={5}>
+                    <Image src={Todo} responsive />
+                    <h3>React Todo</h3>
+                    <Button
+                      bsStyle="primary"
+                      className="btn-margin"
+                      onClick={this.login.bind(this)}
+                    >
+                      Log In To Continue
+                  </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          )
+        }
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { makeMainRoutes } from './routes';
+
+const routes = makeMainRoutes();
+
+ReactDOM.render(routes, document.getElementById('root'));
+
+serviceWorker.unregister();
